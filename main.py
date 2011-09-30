da597d5c663d3ab88ed04f4b54fc8155eab02169
Fred Pauchet <fpauchet@gmail.com>
2011-09-30 15:44:39 +0200

    jquery integration

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import cgi
+import os
 
+from google.appengine.ext.webapp import template
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 class MainPage(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write("""
-          <html>
-            <body>
-              <form action="/peerReview" method="post">
-		<div>Activity number <input type="text" name="reviewName" cols="60" /></div>
-		<div>Activity Date (format YYYY-MM-DD)<input type="text" name="reviewDate" cols="60" /></div>
-                <div><textarea name="content" rows="10" cols="60"></textarea></div>
-                <div><input type="submit" value="Send data"></div>
-              </form>
-            </body>
-          </html>""")
+    def get(self): 
+    	templates_values = {'greetings':'Hello!', 'url':'http://rizinami.appspot.com'}
+    	path = os.path.join(os.path.dirname(__file__), 'index.html')
+    	self.response.out.write(template.render(path,templates_values))
 
+class Medic():
+	def __init__(self, rizivnummer):
+		self.riziv = rizivnummer
+
+	def __unicode__(self):
+		return self.riziv
+
+	def __str__(self):
+		return self.riziv
 
 class PeerReview(webapp.RequestHandler):
 	def post(self):
@@ -26,11 +29,13 @@ class PeerReview(webapp.RequestHandler):
 		content = cgi.escape(self.request.get('content'))
 		content = content.split()
 		
-		self.response.out.write('<html><body>You wrote:')
+		self.response.out.write('<html><body>Results:')
 		self.response.out.write('<ul>')
 		
-		for rz in content:
-			self.response.out.write('<li>' + activityNumber + ';' + rz + ';;1;1;' + activityDate + '</li>')
+		meds = [ Medic(rz) for rz in content]
+
+		for r in meds:
+			self.response.out.write('<li>' + activityNumber + ';' + unicode(r) + ';;1;1;' + activityDate + '</li>')
 		
 		self.response.out.write('</ul>')
 		self.response.out.write('</pre></body></html>')
